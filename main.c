9108a895880c35e56139a637cb0f60cebab598a2
Iasmin Silva <iasminluise14@gmail.com>
2026-03-30 13:42:36 -0300

    Remove duplicate print statement in main function
    Removed duplicate print statement and adjusted formatting.

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 
 int main(){
     
+    printf("Exibir números naturais: ");  //impressão do texto
+
     //declaração feita dentro do laço de repetição for
     //essa declaração é permitida quando se utiliza apenas dentro do laço de repetição
-    
-    printf("Exibir números naturais: ");  //impressão do texto
     
     for(int i=0; i<=4; i++){  //(inicialização; condição; finalização)
     
